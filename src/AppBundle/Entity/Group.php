8e8089a9e62e3b03e1472c6944fe933d4eca90b4
Sergio Gómez <sergio@uco.es>
2015-03-30 19:24:16 +0200

    Movido User, UserAdmin, Group, GroupAdmin  a AppBundle

---
+++ src/AppBundle/Entity/Group.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * This file is part of the <name> project.
+ *
+ * (c) <yourname> <youremail>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AppBundle\Entity;
+use Doctrine\ORM\Mapping as ORM;
+use Sonata\UserBundle\Entity\BaseGroup;
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="fos_group")
+ */
+class Group extends BaseGroup
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+}
